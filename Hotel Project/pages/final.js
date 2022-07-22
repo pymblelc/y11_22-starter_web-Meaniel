0f45cdfcfad39d05ccc45c51fa187efd953ce7ef
Melanie Sun <melaniesun@Melanies-MacBook-Air-2.local>
2022-07-22 10:13:24 +1000

    yes

--- Hotel Project/pages/final.js
+++
@@ -1,5 +0,0 @@
-let roomText = document.getElementById("roomText");
-let roomNumber = Math.floor(Math.random() * 1600);
-
-// generates a random room number
-roomText.innerHTML = 'Your room number is: ' + roomNumber;
